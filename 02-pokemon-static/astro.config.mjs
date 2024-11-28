23fd70ee9ad4502f0b2a66bb31c2b56346e0c385
samuraiOrDev <vm.ordiales.garcia@gmail.com>
2024-11-28 13:05:33 +0100

    Adaptador node

--- 02-pokemon-static/astro.config.mjs
+++ 02-pokemon-static/astro.config.mjs
@@ -3,11 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-import vercel from '@astrojs/vercel/serverless';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: vercel()
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
