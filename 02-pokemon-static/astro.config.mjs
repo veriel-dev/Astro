2a1f8c4e7508c7fccfca58add4c08945b3d0b256
samuraiOrDev <vm.ordiales.garcia@gmail.com>
2024-11-28 12:49:12 +0100

    Cambios para posibles despliegues en vercel

--- 02-pokemon-static/astro.config.mjs
+++ 02-pokemon-static/astro.config.mjs
@@ -9,7 +9,5 @@ import vercel from '@astrojs/vercel/serverless';
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: vercel({
-    functionPerRoute: true,
-  })
+  adapter: vercel()
 });
